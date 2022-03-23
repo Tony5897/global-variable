08d1bb197e027c8e3f7f7e6299f01d2613fae61f
tonym <tony.martinez5897@gmail.com>
2022-03-23 09:55:15 -0700

    initial Commit: global and local scope exercise
    Co-authored-by: Emilio Gonzales <emiliog1184@gmail.com>
    co-authored-by: Jarret Jeter <jarretjeter@yahoo.com>

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+// let globalString = "This is a global variable";
+
+// function sampleFunction() {
+//   alert(globalString);
+//   globalString = "This is a global variable update!!";
+//   alert(globalString);
+// }
+
+// alert(globalString);
+// sampleFunction();
+// alert(globalString);
+
+// ///////////////////////////////////////////////
+
+// function sampleFunction() {
+//   let localString = "This is a local variable";
+//   alert(localString);
+//   localString = "This is a local variable update!!";
+//   alert(localString);
+// }
+
+// sampleFunction();
+// alert(localString);
+
+// $(document).ready(function() {
+
+//   $("div#click-one").click(function() {
+//     const whatToSay = "Hello!";
+//     alert(whatToSay);
+//   });
+
+//   $("div#click-two").click(function() {
+//     const whatToSay = "Hello!";
+//     whatToSay = "hi"
+//     alert(whatToSay);
+//   });
+// });
+
+// function sampleFunction() {
+//   localString = "This is a local variable";
+//   alert(localString);
+//   localString = "This is a local variable update!!";
+//   alert(localString);
+// }
+
+// sampleFunction();
+// alert(localString);
